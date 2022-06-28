38690e3f47740d6ff9b4d45a8c1049470d4b6096
Andy DeVane <w.andrew.dev@gmail.com>
2022-06-28 13:42:55 -0400

    add prototype methods for testing

--- main.py
+++ main.py
@@ -1,12 +1,19 @@
+import os
+
+def move_file(current, destination):
+    os.rename(current, destination)
+
 class Plugin:
     # A normal method. It can be called from JavaScript using call_plugin_function("method_1", argument1, argument2)
-    async def method_1(self, *args):
-        pass
+    async def reset_game_files(self, *args):
+        map_path = "/home/deck/Documents/RLmaps/Medieval Rings/"
+        rocket_league_path = "/home/deck/Games/Heroic/rocketleague/TAGame/CookedPCConsole/"
+        move_file(rocket_league_path + "Labs_Underpass_P.upk", map_path + "LethMedievalRings.udk")
+        move_file(map_path + "ORIGINAL_UNDERPASS.upk", rocket_league_path + "Labs_Underpass_P.upk")
 
-    # A normal method. It can be called from JavaScript using call_plugin_function("method_2", argument1, argument2)
-    async def method_2(self, *args):
-        pass
-
-    # Asyncio-compatible long-running code, executed in a task when the plugin is loaded
-    async def _main(self):
-        pass
+    # A normal method. It can be called from JavaScript using call_plugin_function("method_1", argument1, argument2)
+    async def load_map(self, *args):
+        map_path = "/home/deck/Documents/RLmaps/Medieval Rings/"
+        rocket_league_path = "/home/deck/Games/Heroic/rocketleague/TAGame/CookedPCConsole/"
+        move_file(rocket_league_path + "Labs_Underpass_P.upk", map_path + "ORIGINAL_UNDERPASS.upk")
+        move_file(map_path + "LethMedievalRings.udk", rocket_league_path + "Labs_Underpass_P.upk")
